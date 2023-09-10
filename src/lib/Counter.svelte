e2b6e830e0fa02713f37514088d10f803590085c
Manish <m.wire2511@gmail.com>
2023-09-10 20:51:26 +0530

    first commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+  let count = 0;
+  let str = `tou have clicked ${count}`;
+  const increment = () => {
+    count += 1
+}
+$:str = `you have clicked ${count}`;
+</script>
+ 
+<button on:click={increment}>
+  count is {count}
+</button> 
+<h2>{str}</h2>
+ 
